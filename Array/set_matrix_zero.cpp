69d62c473c411f882bb62ed37daa1de333ac480a
BINYANSHU SINGH <binyanshusingh@BINYANSHUs-MacBook-Air.local>
2025-08-06 22:16:41 +0530

    set matrix zero

---
+++ Array/set_matrix_zero.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> temp = matrix;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                for (int k = 0; k < n; k++) {
+                    temp[i][k] = 0;
+                }
+                for (int k = 0; k < m; k++) {
+                    temp[k][j] = 0;
+                }
+            }
+        }
+    }
+    matrix = temp;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+
+    setZeroes(matrix);
+
+    // Print updated matrix
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
